ada6771b8cb525c5939b60ef395aa6d20abe0754
wiula0-ux <wiula0@gmail.com>
2026-07-21 17:41:16 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,10 +28,23 @@ async def receive_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         "𝐘𝐨𝐮𝐫 𝐰𝐨𝐫𝐝𝐬 𝐰𝐢𝐥𝐥 𝐛𝐞 𝐜𝐚𝐫𝐫𝐢𝐞𝐝 𝐢𝐧 𝐬𝐢𝐥𝐞𝐧𝐜𝐞."
     )
-    
+
+  async def reply_to_user(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.message.reply_to_message:
+        replied_id = update.message.reply_to_message.message_id
+
+        if replied_id in users:
+            user_id = users[replied_id]
+
+            await context.bot.send_message(
+                chat_id=user_id,
+                text=update.message.text
+            )
+            
 app = Application.builder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, receive_message))
+app.add_handler(MessageHandler(filters.REPLY & filters.TEXT, reply_to_user))
 
 app.run_polling()
